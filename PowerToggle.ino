356f693048d226abe8eb2fb998dd4cc7c72b30f2
nogirvine <nogirvine@hotmail.com>
2019-03-12 18:31:38 +0000

    Intinal commit of PowerToggle

---
+++ PowerToggle.ino
@@ -0,0 +1,27 @@
+ void setup() {
+  powerToggle = 0;
+}
+
+void loop() {
+  if (irbutton == 12){
+    if (powerToggle == 0){
+      serial.print ("<1>");
+      powerToggle = 1;
+    }
+    else {
+      serial.print ("<0>");
+      powerToggle = 0;
+      }
+  }
+}
+
+
+
+//   if (powerToggle == 1){
+//     serial.print ("<0>");
+//   }
+//   else {
+//     serial.print ("<1>");
+//     powerToggle = 0
+//     return
+//   }
